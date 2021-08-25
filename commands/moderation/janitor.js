bba1b71786413f342936a39949963a180e493652
jiggey1 <fortniitetradezback@gmail.com>
2021-08-25 16:42:47 +0100

    Completed Janitor Command

--- commands/moderation/janitor.js
+++ commands/moderation/janitor.js
@@ -1,4 +1,4 @@
-const { MessageEmbed, Permissions } = require("discord.js")
+const { MessageEmbed, Permissions, MessageFlags } = require("discord.js")
 
 module.exports = {
     name: "janitor",
@@ -6,7 +6,44 @@ module.exports = {
     description: "Purge Command",
     run: async (client, message, args) => {
 
-        // janitor code
+        function errorEmbed(errorMsg) {
+            const errorEmbed = new MessageEmbed()
+            .setAuthor("Playboy", `${global.botPFP}`)
+            .setTitle("An Error Occurred!")
+            .setColor("RED")
+            .setDescription(errorMsg)
+            .setFooter("Unexpected Error!")
+            .setTimestamp()
 
+            message.channel.send({ embeds: [errorEmbed] });
+        }
+
+        if(!message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) {
+            return errorEmbed("You do not have the required permissions to do this!");
+        }
+
+        if(!args[0]) {
+            return errorEmbed("Please give me a number between ``1`` and ``100``. \nThis is the amount of messages I will delete.");
+        }
+
+        if(args[0] == "0") {
+            return errorEmbed("You cannot delete 0 messages. \nPlease enter a higher number and try again.");
+        }
+
+        let deleteAmount = args[0];
+
+        message.channel.bulkDelete(deleteAmount, true).then(msg => {
+            setTimeout(() => msg.delete, 10000)
+        });
+
+        const successEmbed = new MessageEmbed()
+        .setAuthor("Playboy", `${global.botPFP}`)
+        .setTitle(`Whew! I Deleted ${args[0]} messages!`)
+        .setColor("NAVY")
+        .setDescription("If some messages haven't been deleted, it could be because they are older than 14 days. Discord prevents me from deleting messages that far back. Sorry!")
+        .setFooter("Command Executed!")
+        .setTimestamp()
+
+        message.channel.send({ embeds: [successEmbed] });
     }
 }
