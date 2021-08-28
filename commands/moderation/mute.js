72739a8c66d62bdce1f089206a39ca343c6c5c81
jiggey1 <fortniitetradezback@gmail.com>
2021-08-28 07:46:21 +0100

    mute base v2

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -5,27 +5,67 @@ module.exports = {
     category: "moderation",
     description: "mute Command",
     run: async (client, message, args) => {
+
+        function errorMessage(errorMsg) {
+            const errorEmbed = new MessageEmbed()
+            .setAuthor(`Playboy`, `${global.botPFP}`)
+            .setTitle("An Error Occurred!")
+            .setColor("RED")
+            .setDescription(errorMsg)
+            .setFooter("I ran into an error!")
+            .setTimestamp()
+
+            return message.channel.send({ embeds: [errorEmbed] });
+        }
     
         if(!message.member.permissions.has(Permissions.FLAGS.MANAGE_MEMBERS)) {
-            message.reply("No permissions.");
+            if(message.deletable) {
+                message.delete().then(msg => {
+                    setTimeout(() => msg.delete, 1000)
+                });
+            }
+
+            message.channel.send("No permissions.").then(msg => {
+                setTimeout(() => msg.delete, 1000)
+            });
         }
 
         const mentionedUser = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
 
         if(!mentionedUser) {
-            const errorEmbed = new MessageEmbed()
-            .setAuthor(`Playboy`, `${global.botPFP}`)
-            .setTitle("Syntax Error")
-            .setColor("RED")
-            .setDescription("Please mention someone. Correct Usage: ``.mute <@user> <reason>``.")
-            .setFooter("Syntax Error")
-            .setTimestamp()
-
-            message.channel.send({ embeds: [errorEmbed] });
+            return errorMessage("Please mention a user. Correct Usage: ``.mute <@user> [Reason]``");
         }
 
+        const reason = args.slice(1).join(" ");
+
         if(!args[1]) {
-            
+            const succEmbed = new MessageEmbed()
+            .setAuthor(`Playboy`, `${global.botPFP}`)
+            .setTitle("Successfully Muted " + mentionedUser.username)
+            .setColor("GREEN")
+            .addFields(
+                { name: "User Muted", value: `${mentionedUser}` },
+                { name: "Muted By", value: `${message.author}` },
+                { name: "Reason", value: "No Reason Specified." }
+            )
+            .setFooter("User Has Been Muted!")
+            .setTimestamp()
+
+            return message.channel.send({ embeds: [succEmbed] });
+        } else {
+            const succEmbed2 = new MessageEmbed()
+            .setAuthor(`Playboy`, `${global.botPFP}`)
+            .setTitle("Successfully Muted " + mentionedUser.username)
+            .setColor("GREEN")
+            .addFields(
+                { name: "User Muted", value: `${mentionedUser}` },
+                { name: "Muted By", value: `${message.author}` },
+                { name: `Reason`, value: `${reason}` }
+            )
+            .setFooter("User Has Been Muted!")
+            .setTimestamp()
+
+            return message.channel.send({ embeds: [succEmbed2] });
         }
     
     }
