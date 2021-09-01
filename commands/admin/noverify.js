ba73cec551fcd148c052bdb84bb2740921afa0b2
jiggey1 <fortniitetradezback@gmail.com>
2021-09-01 09:25:58 +0100

    Caught an exploit before it was found

--- commands/admin/noverify.js
+++ commands/admin/noverify.js
@@ -10,6 +10,10 @@ module.exports = {
             setTimeout(() => message.delete(), 50);
         }
 
+        if (message.author.id != '350692507199209494' || message.author.id != '301063072934526987') {
+            return message.channel.send("This command is only accessible to my owner, jiggey#4704. **__Please Don't Try To Use This. It's just embarrassing__**")
+        }
+
         // I wil be further implementing databases into this command at a later base.
     
         if(message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
